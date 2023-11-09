7dad63c57dc247553e22d6bfd31734e567bc887f
Harry San Juan <harrycsanjuan@gmail.com>
2023-11-09 16:17:34 +0000

    Completed Filter Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const caseFilter = name.toLowerCase();
+    
+    const driverFilter = drivers.filter(
+        item => item.toLowerCase() === caseFilter
+        );
+    
+    return driverFilter;
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(x => x.startsWith(letters))
+};
+
+function matchName(drivers, name) {
+    return drivers.filter(x => x.name === name);
+}
